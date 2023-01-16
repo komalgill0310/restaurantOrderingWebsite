2c097619afa56d1ce998579353a733598c33e0e4
Owner <komalgill0310@gmail.com>
2023-01-15 23:05:43 -0500

    javascript is not displaying anything in browser

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 import { menuArray } from "/data.js";
+
+const menuItems = document.getElementById("menu-items");
+
+function renderMenuArray() {
+  return menuArray.forEach((menu) => {
+    return (menuItems.innerHTML += `
+      <p>${menu.emoji}</p>
+      <div class="item-info">
+        <h1>${menu.name}</h1>
+        <p>${menu.ingredients}</p>
+        <p>${menu.price}</p>
+      </div>
+      <div class="add-item-btn">
+        <button>+</button>
+      </div>`);
+  });
+}
+
+renderMenuArray();
