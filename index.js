828a4e3744087417b1508a1aa577afb7cda96684
Owner <komalgill0310@gmail.com>
2023-01-16 18:30:12 -0500

    able to display the menu item's name and price at the bottom of the page, when click on '+ button

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ import { menuArray } from "/data.js";
 
 document.addEventListener("click", (e) => {
   if (e.target.dataset.add) {
-    console.log(e.target.dataset.add);
+    handleAddItemClick(e.target.dataset.add);
   }
 });
+
+function handleAddItemClick(menuId) {
+  console.log(menuId);
+  document.querySelector(".your-order-text").style.display = "block";
+  const orderedItems = document.getElementById("ordered-items");
+  orderedItems.innerHTML += `
+  <h1>${menuArray[menuId].name}</h1>
+  <p>$${menuArray[menuId].price}</p>`;
+}
 
 function getMenuItems() {
   let menuHtml = "";
@@ -13,7 +22,7 @@ function getMenuItems() {
     <div class="item-info">
       <h1>${menu.name}</h1>
       <p>${menu.ingredients}</p>
-      <p>${menu.price}</p>
+      <p>$${menu.price}</p>
     </div>
     <div classs="add-item-btn">
       <button data-add="${menu.id}">+</button>      
